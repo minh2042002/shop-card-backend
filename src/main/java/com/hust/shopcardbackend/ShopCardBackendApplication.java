bcb68dfedabdf90ef480d71506024a137ea8e6ae
ducminh204 <ducminh2042002@gmail.com>
2022-10-25 13:53:37 +0700

    add folder

---
+++ src/main/java/com/hust/shopcardbackend/ShopCardBackendApplication.java
@@ -0,0 +1,13 @@
+package com.hust.shopcardbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopCardBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopCardBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hust/shopcardbackend/ShopCardBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hust.shopcardbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopCardBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
